f38fb6d9e980d3013864ee50a3fd89f4b09f6acd
cara28 <andresr28@hotmail.es>
2020-01-18 12:32:53 -0600

    Subiendo Todo

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lands
+{
+    using Views;
+    public partial class App : Application
+    {
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage (new LoginPage());
+        }
+        #endregion
+
+        #region Metodos 
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        { 
+        }
+        
+        
+        #endregion
+
+
+    }
+}
